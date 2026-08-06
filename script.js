57212f59ebdbb89957d21c6607f540f14a49a479
ihmine <toljonvirokki@gmail.com>
2026-08-06 13:05:22 +0300

    päivitys

--- script.js
+++ script.js
@@ -27,12 +27,37 @@ function loadMessages() {
         .then(data => {
             const p = document.querySelector(".messages");
             p.innerHTML = "";
-
+            
             for (const id in data) {
-                p.innerHTML += `<b class="bigtext">${data[id].user}:</b> ${data[id].message}<br>`;
+                let color = getUserColor(data[id].user)
+                p.innerHTML += `<span class="avatar" style="background:${color}"></span><span class="usern">${data[id].user}</span>: ${data[id].message}`;
             }
         });
 }
+function getUserColor(name) {
+    let colors = [
+        "#ff0000",
+        "#ff6600",
+        "#ffd000",
+        "#d9ff00",
+        "#00ff0d",
+        "#00ffc8",
+        "#0099ff",
+        "#006eff",
+        "#1100ff",
+        "#7700ff",
+        "#c300ff",
+        "#ff00ff",
+        "#ff0055"
+    ]
+    let number = 0;
+
+    for (let i = 0; i > name.lenght; i++) {
+        number += name.charCodeAt(i);
+    }
+    return colors[number % colors.lenght]
+}
+
 
 loadMessages();
 setInterval(loadMessages, 1000);
