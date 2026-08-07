0dd20c3c192e38efe988cfc1434ac8442c133827
ihmine <toljonvirokki@gmail.com>
2026-08-07 09:39:55 +0300

    Update

--- login.js
+++ login.js
@@ -8,6 +8,23 @@ const firebaseConfig = {
     databaseURL: "https://viljonsivu-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "viljonsivu"
 };
+import {
+    GithubAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const provider = new GithubAuthProvider();
+
+document.getElementById("githubLogin").onclick = () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log("Kirjauduttu:", result.user);
+            window.location.href = "success.html";
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
 
 const app2 = initializeApp(firebaseConfig);
 const auth2 = getAuth(app2);
